c519e32d7e79dd38193b6ccce85ee1d830ea4b5e
riyan <riyanbagasdp@student.uns.ac.id>
2024-12-26 09:36:22 +0700

    datatable

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -302,6 +302,10 @@
             <script src="../assets/static/js/components/dark.js"></script>
             <script src="../assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
             <script src="../assets/compiled/js/app.js"></script>
+            <script src="../assets/extensions/jquery/jquery.min.js"></script>
+            <script src="../assets/extensions/datatables.net/js/jquery.dataTables.min.js"></script>
+            <script src="../assets/extensions/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
+            <script src="../assets/static/js/pages/datatables.js"></script>
             <!-- Need: Apexcharts -->
 
             <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
